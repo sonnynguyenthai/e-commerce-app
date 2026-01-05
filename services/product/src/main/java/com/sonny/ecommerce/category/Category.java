48cf7f732da3ccf3d76065b9368810741464846b
Nguyễn Thái Son Ny <134358623+sonnynguyenthai@users.noreply.github.com>
2026-01-05 13:59:45 +1100

    feat: init product service

---
+++ services/product/src/main/java/com/sonny/ecommerce/category/Category.java
@@ -0,0 +1,25 @@
+package com.sonny.ecommerce.category;
+
+import com.sonny.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
---
+++ services/product/src/main/java/com/sonny/ecommerce/product/Product.java
@@ -0,0 +1,27 @@
+package com.sonny.ecommerce.product;
+
+import com.sonny.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private Double quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
